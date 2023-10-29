fe6a1771ca20eb042d6a6c8028d489302df5ff30
kgw <rladndn1@naver.com>
2023-10-29 10:45:20 +0900

    안드로이드 제공 기능 - Intent로 활용(전화 DIAL)

--- app/src/main/java/kr/ac/wku/activity_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/activity_20231029/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.ac.wku.activity_20231029
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -19,7 +20,9 @@ class MainActivity : AppCompatActivity() {
 //            화면 이동시키기 : Intent 클래스 활용
 //            1. 어디서 출발해서 어디로 도착할지 정보 명시(티켓)
 
-            val myIntent = Intent( this, SecondActivity::class.java)
+            val myIntent = Intent(
+
+                this, SecondActivity::class.java)
 
 //            2. myIntent에 적힌대로 실제 화면 이동 코드 필요
             startActivity(myIntent)
@@ -43,6 +46,21 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(myIntent)
 
+        }
+
+//        전화 걸기 버튼 동작
+        binding.btnPhoneCall.setOnClickListener {
+
+//            입력한 전화번호 추출(변수 저장)
+            val inputPhoneNum = binding.edtPhoneNum.text.toString()
+
+//            어디에 전화를 걸지, 번호 정보를 기록하는 Uri
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+//            전화 앱에 전화번호를 들고 이동
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
         }
     }
 }
