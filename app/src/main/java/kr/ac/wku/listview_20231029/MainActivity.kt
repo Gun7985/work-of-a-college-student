e71f807c1564b9ad385912a5de6e522951b429b2
kgw <rladndn1@naver.com>
2023-10-29 14:06:54 +0900

    LongClickEvent + 목록 변동 처리 - notifyDataSetCanged 반드시 설정

--- app/src/main/java/kr/ac/wku/listview_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/listview_20231029/MainActivity.kt
@@ -45,6 +45,25 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "${clickedStd.name} : ${clickedStd.phoneNum}", Toast.LENGTH_SHORT).show()
             
         }
+
+//        한명의 학생을 오래 클릭하면 -> 해당 학생 삭제
+
+        binding.studentListView.setOnItemLongClickListener { adapterView, view, position, l ->
+
+//            오래 클린된 학생 -> (목록에서) 삭제
+
+            mStudentList.removeAt(position) // 내용물 변경 발생
+
+//            어댑터에게 통보
+            mStdAdapter.notifyDataSetChanged()
+
+
+//            LongClick 이벤트는 Bool 타입의 리턴값을 받도록 되어있음.
+
+            return@setOnItemLongClickListener true
+
+
+        }
         
     }
 }
