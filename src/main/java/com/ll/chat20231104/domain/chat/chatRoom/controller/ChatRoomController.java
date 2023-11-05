af6951809e74ef1014e32f71017275a6e9cff5fb
Gun7985 <rladndn1@naver.com>
2023-11-05 14:30:18 +0900

    채팅메세지 ajax

--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -1,14 +1,15 @@
 package com.ll.chat20231104.domain.chat.chatRoom.controller;
 
+import com.ll.chat20231104.domain.chat.chatRoom.entity.ChatMessage;
 import com.ll.chat20231104.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chat20231104.domain.chat.chatRoom.service.ChatRoomService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,21 +18,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
-
     @GetMapping("/{roomId}")
     public String showRoom(
-        @PathVariable final long roomId,
-        final String writerName,
-        Model model
-    ){
+            @PathVariable final long roomId,
+            final String writerName,
+            Model model
+    ) {
         ChatRoom room = chatRoomService.findById(roomId).get();
         model.addAttribute("room", room);
-
         return "domain/chat/chatRoom/room";
     }
-
     @GetMapping("/make")
-    public String showMake(){
+    public String showMake() {
         return "domain/chat/chatRoom/make";
     }
     @PostMapping("/make")
@@ -39,26 +37,35 @@ public class ChatRoomController {
             final String name
     ) {
         chatRoomService.make(name);
-
         return "redirect:/chat/room/list";
     }
-
     @GetMapping("/list")
     public String showList(Model model) {
         List<ChatRoom> chatRooms = chatRoomService.findAll();
         model.addAttribute("chatRooms", chatRooms);
-
         return "domain/chat/chatRoom/list";
     }
 
-    @PostMapping("/{roomId}/write")
-    public String write(
-            @PathVariable final long roomId,
-            final String writerName,
-            final String content
-    ) {
-        chatRoomService.write(roomId, writerName, content);
+    @Setter
+    @Getter
+    public static class WriteRequestBody {
+        private String writerName;
+        private String content;
+    }
 
-        return "redirect:/chat/room/" + roomId;
+    @Getter
+    @AllArgsConstructor
+    public static class WriteResponseBody {
+        private Long chatMessageId;
+    }
+    @PostMapping("/{roomId}/write")
+            @ResponseBody
+            public WriteResponseBody write(
+                    @PathVariable final long roomId,
+            @RequestBody final WriteRequestBody requestBody
+    ) {
+        ChatMessage chatMessage = chatRoomService.write(roomId, requestBody.getWriterName(), requestBody.getContent());
+
+        return new WriteResponseBody(chatMessage.getId());
     }
 }
