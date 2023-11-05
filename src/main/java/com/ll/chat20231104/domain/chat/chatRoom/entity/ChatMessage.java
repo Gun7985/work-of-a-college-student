c6afe84e7d27741aac8ad9642db65e183baa23c1
Gun7985 <rladndn1@naver.com>
2023-11-05 13:51:13 +0900

    BaseEntity

--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/entity/ChatMessage.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/entity/ChatMessage.java
@@ -7,11 +7,7 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
@@ -22,21 +18,11 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @SuperBuilder
 @ToString(callSuper = true)
-public class ChatRoom extends BaseEntity {
-    private String name;
+public class ChatMessage extends BaseEntity {
+    @ManyToOne
+    private ChatRoom chatRoom;
 
-    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    @ToString.Exclude
-    private List<ChatMessage> chatMessages = new ArrayList<>();
+    private String writerName;
 
-    public void writeMessage(String writerName, String content) {
-        ChatMessage chatMessage = ChatMessage
-                .builder()
-                .chatRoom(this)
-                .writerName(writerName)
-                .content(content)
-                .build();
-        chatMessages.add(chatMessage);
-    }
+    private String content;
 }
--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/service/ChatRoomService.java
@@ -4,23 +4,38 @@ import com.ll.chat20231104.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chat20231104.domain.chat.chatRoom.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
 
-    public void make(String name) {
+    @Transactional
+    public ChatRoom make(String name) {
         ChatRoom chatRoom = ChatRoom.builder()
                 .name(name)
                 .build();
 
         chatRoomRepository.save(chatRoom);
+        return chatRoom;
     }
 
     public List<ChatRoom> findAll() {
         return chatRoomRepository.findAll();
+    }
+
+    @Transactional
+    public void write(long roomId, String writerName, String content) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).get();
+
+        chatRoom.writeMessage(writerName, content);
+    }
+
+    public Optional<ChatRoom> findById(long roomId) {
+        return chatRoomRepository.findById(roomId);
     }
 }
