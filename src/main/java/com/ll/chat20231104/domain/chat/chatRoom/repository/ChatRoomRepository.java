f3e91e59a5c9d628a7a0569142f79c0f4ec35b6d
Gun7985 <rladndn1@naver.com>
2023-11-05 14:04:36 +0900

    form check

--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/repository/ChatRoomRepository.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/repository/ChatRoomRepository.java
@@ -1,7 +1,6 @@
 package com.ll.chat20231104.domain.chat.chatRoom.repository;
 
 import com.ll.chat20231104.domain.chat.chatRoom.entity.ChatRoom;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
